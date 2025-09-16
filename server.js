185ed8654a9d76d2bcf1ad743036389d55c5d859
VinothGVKumbam <your-email@domain.com>
2025-09-16 11:50:07 +0530

    Removed unwanted files

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
-import adminRouter from "./routes/adminRoute.js";
-import doctorRouter from "./routes/doctorRoute.js";
-import userRouter from "./routes/userRoute.js";
 
+import userRouter from "./routes/userRoute.js";
 import touradminRouter from "./routes/tourAdminRoute.js";
 import tourRouter from "./routes/tourRoute.js";
 
@@ -25,11 +23,11 @@ app.use(cors());
 //API endpoints
 
 //Admin
-app.use("/api/admin", adminRouter);
+
 app.use("/api/touradmin", touradminRouter);
 
 //Tour
-app.use("/api/doctor", doctorRouter);
+
 app.use("/api/tour", tourRouter);
 
 app.use("/api/user", userRouter);
