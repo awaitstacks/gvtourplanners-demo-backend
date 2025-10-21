7db7425046a2a6d44788fdb67689c1bbac92d40a
VinothGVKumbam <your-email@domain.com>
2025-10-21 18:19:37 +0530

    No changes

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,19 +1,3 @@
-//Original version
-
-// import multer from "multer";
-
-// const storage = multer.diskStorage({
-//   filename: function (req, file, callback) {
-//     callback(null, file.originalname);
-//   },
-// });
-// const upload = multer({ storage });
-// export default upload;
-
-//GVTOUR Version by gpt
-
-// middlewares/multer.js
-
 import multer from "multer";
 
 const storage = multer.diskStorage({
